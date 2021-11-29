37959f6147cb0916f3bb5ec9edd2dc4ad8cf6cc7
nlicitra <nickerous@gmail.com>
2021-11-29 16:33:20 -0500

    Fixed vite rollup config to use correct base path

---
+++ src/blue.ts
@@ -0,0 +1,85 @@
+import {
+  MeshBuilder,
+  Engine,
+  Scene,
+  FreeCamera,
+  Vector3,
+  StandardMaterial,
+  Color3,
+  PointerEventTypes,
+  Mesh,
+  ShadowGenerator,
+  DirectionalLight,
+  Color4,
+} from '@babylonjs/core';
+
+interface LinkMesh extends Mesh {
+  href: string;
+}
+
+const canvas = document.querySelector<HTMLCanvasElement>('#renderCanvas');
+
+const engine = new Engine(canvas, true);
+
+const scene = new Scene(engine);
+scene.clearColor = Color4.FromHexString('#4287f5');
+const camera = new FreeCamera('camera', new Vector3(0, 1, -10), scene);
+camera.setTarget(Vector3.Zero());
+camera.attachControl(true);
+const light = new DirectionalLight('dir-light', new Vector3(0, -2, 0), scene);
+light.position = new Vector3(0, 10, 0);
+
+const ground = MeshBuilder.CreateGround('ground', {width: 100, height: 100}, scene);
+ground.receiveShadows = true;
+
+function createLinkCube(hexColor: string, href: string, position: Vector3) {
+  const mat = new StandardMaterial(`cube-mat-${hexColor}`, scene);
+  const color = Color3.FromHexString(hexColor);
+  mat.diffuseColor = color;
+  mat.emissiveColor = color;
+
+  const box = MeshBuilder.CreateBox('box', {size: 2}, scene) as LinkMesh;
+  box.position = position;
+  box.material = mat;
+  box.href = href;
+  box.renderOutline = true;
+  box.outlineColor = Color3.Black();
+  return box;
+}
+
+const hubBox = createLinkCube('#999999', '/', new Vector3(0, 2, 0));
+
+const shadowGenerator = new ShadowGenerator(2048, light);
+shadowGenerator.usePoissonSampling = true;
+shadowGenerator.useExponentialShadowMap = true;
+shadowGenerator.addShadowCaster(hubBox);
+
+scene.registerBeforeRender(() => {
+  hubBox.rotation.y += 0.01;
+});
+
+engine.runRenderLoop(() => {
+  scene.render();
+});
+
+scene.onPointerObservable.add((pointerInfo) => {
+  if (pointerInfo.type === PointerEventTypes.POINTERDOWN) {
+    if (pointerInfo.pickInfo?.hit) {
+      const mesh = pointerInfo.pickInfo.pickedMesh! as LinkMesh;
+      if (mesh.href) {
+        window.location.href = mesh.href;
+      }
+    }
+  }
+});
+
+let promise = scene.createDefaultXRExperienceAsync({
+  floorMeshes: [ground],
+});
+
+// @ts-ignore
+navigator.xr.addEventListener('sessiongranted', async (event) => {
+  console.log('SESSION GRANTED', event);
+  const defaultExperience = await promise;
+  defaultExperience.baseExperience.enterXRAsync('immersive-vr', 'local-floor');
+});
